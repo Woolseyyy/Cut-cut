a726a8e8ee1bfedf41e76a0ca4839fa113e7e12c
woolseyyy <woolsey@zju.edu.cn>
2017-06-28 19:56:17 +0800

    finish article api

--- routes/index.js
+++ routes/index.js
@@ -4,25 +4,34 @@ var spiders = require('../spider/index');
 var News = require('../db/news');
 
 /* GET home page. */
-
-router.get('/list', function(req, res, next) {
+var cors = function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:8080");
     //res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type,If-Modified-Since");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By",' 3.2.1');
     next();
-});
+};
 
 router.get('/', function(req, res, next) {
   spiders();
   res.json({ok:'ok'});
 });
 
+router.get('/list', cors);
 router.get('/list', function(req, res, next){
   News.find({},function(err, news){
       res.json({news:news});
   })
 });
+
+router.get('/article', cors);
+router.get('/article', function(req, res, next){
+  var id = req.query.id;
+  News.findOne({_id:id},function(err, news){
+        res.json({news:news});
+    })
+});
+
 
 module.exports = router;
